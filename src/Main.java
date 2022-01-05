beea89922e5bf8302a7710416121a65d7e61d573
Mikhail Voroshilov <m.s.voroshilovv@gmail.com>
2022-01-05 17:18:23 +0800

    two popers

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
 public class Main {
     public static void main(String [] args) {
         CreditPayment service = new CreditPayment();
-        double mounth = 24;
+        double mounth = 12;
         double sum = 1_000_000;
         double bmi = service.calculate(mounth, sum);
         System.out.println(bmi);
+
+        mounth = 24;
+        double bms = service.calculate(mounth, sum);
+        System.out.println(bms);
+
+        mounth = 36;
+        double bmw = service.calculate(mounth, sum);
+        System.out.println(bmw);
     }
+
 }
